7205b38b33a15975d37c25a9ed8e02ca05f49c38
NanoChip <NanoChip@DESKTOP-2104TCA>
2024-12-26 21:11:19 +0200

    Q1

---
+++ Assignment05/Program.cs
@@ -0,0 +1,45 @@
+﻿namespace Assignment05
+{
+
+    //enum TheDaysOfWeek
+    //{
+       
+       
+    //    monday =1,
+    //    tuesday,
+    //    wednesday,
+    //    thursday,
+    //    friday ,
+    //    saturday, 
+    //    sunday
+    //}
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Q1 : Create an enum called "WeekDays" with the days of the week (Monday to Sunday) as its members.Then, write a C# program that prints out all the days of the week using this enum.
+
+
+            //Create an enum called "WeekDays" with the days of the week (Monday to Sunday) as its members.
+            //Then, write a C# program that prints out all the days of the week using this enum.
+
+            //foreach (TheDaysOfWeek val in Enum.GetValues(typeof(TheDaysOfWeek)))
+            //{
+            //    Console.WriteLine(val);
+            //}
+
+
+
+
+
+            //for (int i = 0; i <=7  ; i++)
+            //{
+            //    Console.WriteLine(TheDaysOfWeek();
+            //} 
+            #endregion
+
+
+
+        }
+    }
+}
