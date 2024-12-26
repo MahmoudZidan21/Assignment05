3718fe407a2b16255a659bc04f9f0c40fb110fb6
NanoChip <NanoChip@DESKTOP-2104TCA>
2024-12-26 09:17:47 +0200

    Nullable Value Type

--- Demo5/Program.cs
+++ Demo5/Program.cs
@@ -5,6 +5,7 @@
         static void Main(string[] args)
         {
 
+            #region Boxing And UnBoxing
 
             // Boxing And UnBoxing
 
@@ -44,6 +45,84 @@
             // UnBoxing
             // Dog = (Dog) Animal => Animal Is Dog
             // 
+
+
+            #endregion
+
+
+            #region Nullable Value Type
+            // Nullable Types 
+            // Value Type
+            // Reference Type
+
+            //int X = 8;
+            //X = null; // Invalid
+
+            // NullAble<int> : Allow Int Valeu or Null As Valid value  
+            //int? Age = 20; 
+            //Age = null;
+
+            // NullAble<double> : Allow Int Valeu or Null As Valid value
+            //double? Salary = 4000.5;
+            //Salary = null;
+
+
+            //int X = 5;
+            //// Y : Nullable<int>
+            //// Y : Allow int Value or Null
+
+            //int? Y = /*(int)*/X;
+            //// Imlicit Casting
+            //// Safe CAsting
+
+            //Console.WriteLine(Y);
+
+
+
+            //  int?X = 5;
+            //X = null;
+            // Y : Nullable<int>
+            // Y : Allow int Value only 
+
+            // int Y = (int)X;
+            // Explicit Casting
+            // UnSafe CAsting
+            //May throw Exeption
+
+            //int Y;
+
+            //// Defensive Code
+            //if (X != null) 
+            //          Y = (int)X;
+            //else 
+            //    Y = 0;
+            //// 
+            //if (X is not null)
+            //    Y = (int)X;
+            //else
+            //    Y = 0;
+
+            // =============================
+
+            //if (X.HasValue) 
+            //    Y = X.Value;
+            //else
+            //    Y = 0;
+
+            //Console.WriteLine(Y);
+
+            // =======================
+
+            // Null Coalescing Operator
+
+            //Y = X ?? 0;
+
+            //Console.WriteLine(Y);
+
+
+            #endregion
+
+
 
 
 
