12d39f1f7823c18c0e961404d568ed35be3c6de2
NanoChip <NanoChip@DESKTOP-2104TCA>
2024-12-26 08:37:52 +0200

    Boxing And UnBoxing

--- Demo5/Program.cs
+++ Demo5/Program.cs
@@ -4,7 +4,56 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+
+
+            // Boxing And UnBoxing
+
+            // Boxing   : Casting From ValueType TO ReferenceType
+            // UnBoxing : Casting From ReferenceType TO ValueType
+
+
+            //object O1;
+
+            // O1 => Can Referer to Instance of " Object" Or any Instance From Any Type 
+
+            //O1 = 1;
+            //O1 = " Ahmed ";
+
+            //O1 = 3;             // Cast From VAlueType(int) TO ReferenceType (Object) => // Boxing
+            //O1 = 3.5;           //  Cast From VAlueType(double) TO ReferenceType (Object) => // Boxing
+            //O1 = "A";           //  Cast From VAlueType(Char) TO ReferenceType (Object) => // Boxing
+            //O1 = true;          //Cast From VAlueType(bool) TO ReferenceType(Object) => // Boxing
+            //O1 =new DateTime(); //Cast From VAlueType(DateTime) TO ReferenceType(Object) => // Boxing
+
+
+            //int X = 5;
+
+            //object O1 = X; 
+            // Imblicit Casting
+            // Safe Casting 
+            // Boxing 
+            // Parent = Child => Child is A parent 
+            // Animal = Dog => Dog Is Animel 
+
+
+            //object O1 = 3;
+
+            //int X =(int) O1;
+            // Explicit Casting
+            // Unsafe Casting (May throw Exeption )
+            // UnBoxing
+            // Dog = (Dog) Animal => Animal Is Dog
+            // 
+
+
+
+
+
+
+
+
+
+
         }
     }
 }
