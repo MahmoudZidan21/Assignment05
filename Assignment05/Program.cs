342d95e52eb8f3d2acd69cd24aa018820897579d
NanoChip <NanoChip@DESKTOP-2104TCA>
2024-12-27 08:03:32 +0200

    Q2

--- Assignment05/Program.cs
+++ Assignment05/Program.cs
@@ -1,10 +1,12 @@
-﻿namespace Assignment05
+﻿using System.Reflection;
+
+namespace Assignment05
 {
 
     //enum TheDaysOfWeek
     //{
-       
-       
+
+
     //    monday =1,
     //    tuesday,
     //    wednesday,
@@ -13,6 +15,16 @@
     //    saturday, 
     //    sunday
     //}
+
+
+    enum Season
+    {
+        Spring,
+        Summer,
+        Autumn,
+        Winter
+
+    }
     internal class Program
     {
         static void Main(string[] args)
@@ -37,6 +49,51 @@
             //    Console.WriteLine(TheDaysOfWeek();
             //} 
             #endregion
+
+            #region Q2:  //Create an enum called "Season" with the four seasons (Spring, Summer, Autumn, Winter) as its members.
+            //Write a C# program that takes a season name as input from the user and displays the corresponding month range for that season.
+            //Note range for seasons ( spring march to may , summer june to august , autumn September to November ,
+            //winter December to February)
+            //Create an enum called "Season" with the four seasons (Spring, Summer, Autumn, Winter) as its members.
+            //Write a C# program that takes a season name as input from the user and displays the corresponding month range for that season.
+            //Note range for seasons ( spring march to may , summer june to august , autumn September to November ,
+            //winter December to February)
+            //September, October, November, December ,January, February, March, April, May, June, July, August
+
+            //Season Se;
+            //Console.WriteLine(" Enter a Season");
+
+            //Se = (Season)Enum.Parse(typeof(Season), Console.ReadLine(), true);
+
+
+
+            //switch (Se)
+            //{
+            //    case Season.Spring :
+            //        Console.WriteLine("Spring from march to may");
+            //        break;
+
+            //    case Season.Autumn :
+            //        Console.WriteLine("Autom from September to November");
+            //        break;
+
+            //        case Season.Summer :
+            //        Console.WriteLine("summer from  june to august ");
+            //        break;
+            //    case Season.Winter :
+            //        Console.WriteLine("winter from December to February ");
+            //        break;
+
+
+            //}
+
+            #endregion
+
+
+
+
+
+
 
 
 
