4b170b7778dfaceee69d36fbc5d2e2744da0af30
NanoChip <NanoChip@DESKTOP-2104TCA>
2024-12-27 09:41:31 +0200

    Q5

--- Assignment05/Program.cs
+++ Assignment05/Program.cs
@@ -1,4 +1,6 @@
-﻿using System.Reflection;
+﻿using System.Drawing;
+using System.Reflection;
+using System.Threading.Channels;
 
 namespace Assignment05
 {
@@ -17,24 +19,37 @@ namespace Assignment05
     //}
 
 
-    enum Season
+    //enum Season
+    //{
+    //    Spring,
+    //    Summer,
+    //    Autumn,
+    //    Winter
+
+    //}
+
+    //[Flags]
+    //enum Permisions
+    //{
+    //    Read = 8,
+    //    Write = 4,
+    //    Excute = 2,
+    //    Delete = 1
+
+    //}
+
+
+    enum Color
     {
-        Spring,
-        Summer,
-        Autumn,
-        Winter
+        Red ,
+        Green ,
+        Blue 
 
     }
 
-    [Flags]
-    enum Permisions
-    {
-        Read = 8,
-        Write = 4,
-        Excute = 2,
-        Delete = 1
 
-    }
+
+
     internal class Program
     {
         static void Main(string[] args)
@@ -111,6 +126,54 @@ namespace Assignment05
             //Per ^= Permisions.Delete;
             //Console.WriteLine(Per); 
             #endregion
+
+            #region Q5 :Create an enum called "Colors" with the basic colors (Red, Green, Blue) as its members. Write a C# program that takes a color name as input from the user and displays a message indicating whether the input color is a primary color or not.
+
+
+            // . Create an enum called "Colors" with the basic colors (Red, Green, Blue) as its members. Write a C# program that takes a color name as input from the user and displays a message indicating whether the input color is a primary color or not.
+
+            //Color C1;
+
+
+            //Console.WriteLine(" Enter A Color ");
+
+
+            //try
+            //{
+            //    C1 = (Color)Enum.Parse(typeof(Color), Console.ReadLine(), true);
+
+            //    if (C1 == Color.Red)
+            //    {
+            //        Console.WriteLine($"the {C1} is Basic Color");
+            //    }
+            //    else if (C1 == Color.Green)
+            //    {
+            //        Console.WriteLine($"the {C1} is Basic Color");
+            //    }
+            //    else if (C1 == Color.Blue)
+            //    {
+            //        Console.WriteLine($"the {C1} is Basic Color");
+            //    }
+            //    else
+            //    {
+            //        Console.WriteLine($"the {C1} is not a Basic Color");
+            //    }
+
+
+            //}
+            //catch
+            //{
+            //    Console.WriteLine("is not a basic Color");
+            //} 
+            #endregion
+
+
+
+
+
+
+
+
 
 
 
